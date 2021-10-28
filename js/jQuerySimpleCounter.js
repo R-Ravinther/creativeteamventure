06eb643a17dfb81759f51b15deda2d3b736382b5
R-Ravinther <61197918+R-Ravinther@users.noreply.github.com>
2021-10-28 10:37:13 +0530

    upload creative team venture index

---
+++ js/jQuerySimpleCounter.js
@@ -0,0 +1,50 @@
+
+(function($) {
+	$.fn.jQuerySimpleCounter = function( options ) {
+	    settings = $.extend({
+	        start:  0,
+	        end:    100,
+	        easing: 'swing',
+	        duration: 400,
+	        complete: ''
+	    }, options );
+
+	    const thisElement = $(this);
+
+	    $({count: settings.start}).animate({count: settings.end}, {
+			duration: settings.duration,
+			easing: settings.easing,
+			step: function() {
+				mathCount = Math.ceil(this.count);
+				thisElement.text(mathCount);
+			},
+			complete: settings.complete
+		});
+	};
+
+}(jQuery));
+
+
+$('#exp').jQuerySimpleCounter({
+			start:  0,
+	        end:    15,
+			duration: 4000
+		});
+
+$('#satisfy').jQuerySimpleCounter({
+			start:  0,
+	        end:    1000,
+			duration: 4000
+		});
+$('#corpo').jQuerySimpleCounter({
+			start:  0,
+	        end:    130,
+			duration: 4000
+		});
+$('#teamm').jQuerySimpleCounter({
+			start:  0,
+	        end:    20,
+			duration: 4000
+		});
+
+  
